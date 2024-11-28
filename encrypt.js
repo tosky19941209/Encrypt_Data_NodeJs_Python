6f3183aada022f10d7b901f9b094cbe1f8f8c92b
tosky19941209 <felixeliass1994@gmail.com>
2024-11-28 03:27:07 -0500

    feat: python crypto

--- encrypt.js
+++ encrypt.js
@@ -7,21 +7,21 @@ const SECRET_KEY = 'mysecretkey1234';
 function encrypt(data) {
     const ciphertext = CryptoJS.AES.encrypt(JSON.stringify(data), SECRET_KEY).toString();
     return ciphertext;
-}
-
-// Decrypt data
-function decrypt(ciphertext) {
+  }
+  
+  // Decrypt data
+  function decrypt(ciphertext) {
     const bytes = CryptoJS.AES.decrypt(ciphertext, SECRET_KEY);
     const originalData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
     return originalData;
-}
+  }
 
-const data = {
+  const data = {
     message: " hello",
     timeStamp: 100
-}
+  }
 
-encryptedData = encrypt(data)
-decryptedData = decrypt(encryptedData)
-console.log(encryptedData)
-console.log(decryptedData)
+  encryptedData = encrypt(data)
+  decryptedData = decrypt(encryptedData)
+  console.log(encryptedData)
+  console.log(decryptedData)
